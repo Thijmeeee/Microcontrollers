957feb7a0cd6412a8e7a5a58d4480ce7eca36ba6
Ruben Claassen <ruben.claassen@home.nl>
2024-03-02 14:50:18 +0100

    Week 1 opdracht 6 fixed

--- Week1/Opdracht 6/Week 1 Opdracht 6/main.c
+++ Week1/Opdracht 6/Week 1 Opdracht 6/main.c
@@ -1,18 +1,45 @@
-/*
- * Week 1 Opdracht 6.c
- *
- * Created: 02/03/2024 14:35:03
- * Author : ruben
- */ 
-
 #include <avr/io.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+
+#define BIT(x)(1<<(x))
+
+//delays for "ms" milliseconds
+void wait(int ms) {
+	for (int i = 0; i < ms; i++) {
+		_delay_ms(1);
+	}
+}
 
 
-int main(void)
+// switches frequence of light flashes when button C0 is pressed
+int main (void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRD = 0xFF;	//Sets PORTD to output
+	PORTC = 0x01;	//sets PORTC.0 to input
+	int HZ_SPEED = 250;
+	
+	while (1)
+	{
+		// when button C0 is pressed HZ_SPEED switches
+		if (PINC & 0x01)
+		{
+			if (HZ_SPEED == 150)
+			{	
+				HZ_SPEED = 900;
+			}
+			else if (HZ_SPEED == 900)
+			{
+				HZ_SPEED = 150;
+			}
+		}
+		
+		// turns LED D7 on and off and then wait HZ_speed
+		PORTD = BIT(7);
+		_delay_ms(100);
+		PORTD = 0b00000000;
+		
+		wait(HZ_SPEED);
+	}
 }
 
