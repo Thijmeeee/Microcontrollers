6bb59a5c24cf34681e0da5694f0b7f523cf5098c
Thijmeee <t.franken1@student.avans.nl>
2024-03-03 12:20:30 +0100

    Kleine fix in commands van week 2, opdracht b2

--- Week1/Opdracht 2/Week 1 Opdracht 1/main.c
+++ Week1/Opdracht 2/Week 1 Opdracht 1/main.c
@@ -8,12 +8,11 @@ int main(void)
 {
 	DDRD = 0xFF //puts PORTD as output
 	
-	
 	while (1)
 	{
 		PORTD = BIT(6);		//turns on BIT 6  on PORTD
 		_delay_ms(500);
-		PORTD = BIT(7);		//turns on BIT 6  on PORTD
+		PORTD = BIT(7);		//turns on BIT 7  on PORTD
 		_delay_ms(500);
 	}
 	return 0;
